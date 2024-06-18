2b4a7069ad6c6b6e3daffd47a3bf019e5173b3d6
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-06-18 11:28:59 +0300

    added getUsersForSidebar feature

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, sendMessage);
+router.get("/conversations", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
