c797cee05894cfa38a714de67c1643934e4803a0
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-06-18 10:37:38 +0300

    added getMe (profile) feature

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -87,7 +87,7 @@ export const login = async (req: Request, res: Response) => {
       profilePic: user.profilePic,
     });
   } catch (error: any) {
-    console.log("Error in signup controller", error.message);
+    console.log("Error in login controller", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -97,7 +97,24 @@ export const logout = async (req: Request, res: Response) => {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error: any) {
-    console.log("Error in signup controller", error.message);
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMe = async (req: Request, res: Response) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id: req.user.id } });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const { id, fullName, username, profilePic } = user;
+
+    res.status(200).json({ id, fullName, username, profilePic });
+  } catch (error: any) {
+    console.log("Error in getMe controller", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
